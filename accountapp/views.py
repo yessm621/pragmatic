91d42e395e27ea9e8864a182231d56bef0bbc2d0
yessm621 <yessm621@gmail.com>
2021-02-17 16:02:43 +0900

    Initial commit

---
+++ accountapp/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
+
+def hello_world(request):
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
