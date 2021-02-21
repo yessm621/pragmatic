4959ead55820e2331339972c9823f53276f31715
yessm621 <yessm621@gmail.com>
2021-02-21 17:38:49 +0900

    profile app end

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='profile')
-    image = models.ImageField(upload_to='profile/', null=True)
+    image = models.CharField(max_length=255, null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
