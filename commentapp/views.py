5bba076311eed11b3e369125367051151bb54c2f
yessm621 <yessm621@gmail.com>
2021-02-26 10:12:40 +0900

    comment app update, delete

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -29,3 +30,15 @@ class CommentDetailView(DetailView):
     model = Comment
     context_object_name = 'target_comment'
     template_name = 'commentapp/detail.html'
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',
+                       kwargs={'pk': self.object.article.pk})
