9a3bf23759d4e291fe9144045bf6556f0ad56f8e
yessm621 <yessm621@gmail.com>
2021-02-28 19:08:06 +0900

    project appMultipleObjectMixin

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projectapp.models import Project
+
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='project', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.CharField(max_length=20, null=False)
     content = models.TextField(null=True)
